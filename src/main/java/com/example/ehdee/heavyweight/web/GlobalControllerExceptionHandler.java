a53e416321bda34f90a3f54e14cce10b774dad01
eddie <eddie@example.com>
2016-04-13 14:45:17 +0100

    updating exception handling to bring back errors from service

--- src/main/java/com/example/ehdee/heavyweight/web/GlobalControllerExceptionHandler.java
+++ src/main/java/com/example/ehdee/heavyweight/web/GlobalControllerExceptionHandler.java
@@ -4,6 +4,7 @@ package com.example.ehdee.heavyweight.web;
 import com.example.ehdee.heavyweight.exception.HeavyweightServiceDateException;
 import com.example.ehdee.heavyweight.model.HeavyweightResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,18 +15,17 @@ import org.springframework.web.servlet.NoHandlerFoundException;
 @ControllerAdvice
 class GlobalControllerExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     @ExceptionHandler(HeavyweightServiceDateException.class)
     public HeavyweightResponse handleDateError(HeavyweightServiceDateException exception) {
         return new HeavyweightResponse.Builder().withError(exception.getMessage()).build();
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
     @ExceptionHandler({MissingServletRequestParameterException.class, NoHandlerFoundException.class})
-    public HeavyweightResponse handleConflict() {
-        return new HeavyweightResponse.Builder().withError("Unspecified error.").build();
+    public ResponseEntity<String> handleConflict() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad juju.");
     }
 
 }
